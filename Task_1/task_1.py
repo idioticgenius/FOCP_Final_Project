2d91264cc9e71973ff8dea2806652e7b9fe68904
idioticgenius <abhidanthapa@gmail.com>
2024-01-04 15:57:30 +0545

    First draft
    Created basic pizza app outline

---
+++ Task_1/task_1.py
@@ -0,0 +1,29 @@
+def get_pizza_count(display_text):
+    pass
+def get_yes_no_input(display_text):
+    pass
+def calculate_total_price(pizza_count, is_delivery, is_tuesday, used_app ):
+    pass
+
+
+
+def main():
+
+    print("\nBPP Pizza Price Calculator")
+    print("====================================")
+
+    pizza_count = get_pizza_count("How many pizzas ordered? ")\
+
+    is_delivery = get_yes_no_input("Is delivery required? (Y/N) ")
+
+    is_tuesday = get_yes_no_input("Is it Tuesday? (Y/N) ")
+
+    used_app = get_yes_no_input("Did the Customer use the app? (Y/N) ")
+
+    total_price = calculate_total_price(pizza_count, is_delivery, is_tuesday, used_app)
+    print("=====================================")
+    print(f"Total Price: £{total_price:.2f}")
+    print("=====================================")
+
+if __name__ == "__main__":
+    main()
