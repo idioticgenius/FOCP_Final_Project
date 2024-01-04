51e309ef4d685c229338dcfb016f9af5be3639ba
idioticgenius <abhidanthapa@gmail.com>
2024-01-04 16:14:37 +0545

    Created configuration
    Created CONSTANTS to make the app configurable. It will make it easier to update if price and other factors change without having to make edits at different places in the code

--- Task_1/task_1.py
+++ Task_1/task_1.py
@@ -1,3 +1,9 @@
+PIZZA_COST = 12
+DELIVERY_COST = 2.5
+TUESDAY_DISCOUNT = 0.5
+APP_DISCOUNT = 0.25
+DISPLAY_TEXT_PIZZA = "How many pizzas ordered? "
+
 def get_pizza_count(display_text):
     while True:
         try:
@@ -18,16 +24,16 @@ def get_yes_no_input(display_text):
             print("Please input 'Y' or 'N'.")
 
 def calculate_total_price(pizza_count, is_delivery, is_tuesday, used_app ):
-    total_cost = pizza_count * 12
+    total_cost = pizza_count * PIZZA_COST
 
     if is_tuesday.upper() == "Y":
-        total_cost *= 0.5
+        total_cost *= TUESDAY_DISCOUNT
 
     if is_delivery.upper() == "Y" and pizza_count < 5:
-        total_cost *= 2.5
+        total_cost *= DELIVERY_COST
 
     if used_app.upper() == "Y":
-        total_cost -= (total_cost * 0.25)
+        total_cost -= (total_cost * APP_DISCOUNT)
 
     return total_cost
 
@@ -39,16 +45,12 @@ def main():
     print("====================================")
 
     pizza_count = get_pizza_count("How many pizzas ordered? ")
-    #print(pizza_count)
 
     is_delivery = get_yes_no_input("Is delivery required? (Y/N) ")
-    print(is_delivery)
 
     is_tuesday = get_yes_no_input("Is it Tuesday? (Y/N) ")
-    print(is_tuesday)
 
     used_app = get_yes_no_input("Did the Customer use the app? (Y/N) ")
-    print(used_app)
 
     total_price = calculate_total_price(pizza_count, is_delivery, is_tuesday, used_app)
     print("=====================================")
