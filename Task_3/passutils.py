97a97dbb2b2fa0c3e2d3d493c42a1e00e2d002f1
idioticgenius <abhidanthapa@gmail.com>
2024-01-18 23:48:42 +0545

    added docstring in passutils.py

--- Task_3/passutils.py
+++ Task_3/passutils.py
@@ -2,14 +2,29 @@ import hashlib
 import getpass
 
 def hashing(new_password):
-    # Create a new SHA256 hash object of the password
+    """
+    Create a SHA256 hash of the given password.
+
+    Args:
+    new_password (str): The password to be hashed.
+
+    Returns:
+    str: The hexadecimal hash of the password.
+    """
     hash_object = hashlib.sha256(new_password.encode())
 
-    # Get the hexadecimal representation of the hash
-    hex_dig = hash_object.hexdigest()
-    return hex_dig
+    return hash_object.hexdigest()
 
 def validate_password(password):
+    """
+    Validate the given password based on defined criteria.
+
+    Args:
+    password (str): The password to be validated.
+
+    Returns:
+    bool: True if the password meets the criteria, False otherwise.
+    """
     if len(password) < 8 or len(password) > 16:
         print("Password must be at least 8 characters long.")
         return False
@@ -38,12 +53,30 @@ def validate_password(password):
     return True
 
 def get_valid_password(prompt = "Enter password: "):
+    """
+    Prompt the user to enter a password and validate it.
+
+    Args:
+    prompt (str, optional): The prompt to display to the user.
+
+    Returns:
+    str: The validated password.
+    """
     while True:
         new_password = getpass.getpass(prompt)
         if validate_password(new_password):
             return new_password
 
 def confirm_password(password):
+    """
+    Confirm the given password by asking the user to re-enter it.
+
+    Args:
+    password (str): The original password to match.
+
+    Returns:
+    bool: True if the user correctly re-enters the password, False otherwise.
+    """
     for i in range(3):
         confirm_password = getpass.getpass("Confirm Password: ")
         if password == confirm_password:
@@ -54,6 +87,18 @@ def confirm_password(password):
 
 
 def authenticate_user(userbase, username, prompt = "Enter Password: "):
+    """
+    Authenticate a user by verifying their password.
+
+    Args:
+    userbase (dict): A dictionary containing username-password pairs.
+    username (str): The username of the user to authenticate.
+    prompt (str, optional): The prompt to display for password input.
+
+    Returns:
+    bool: True if authentication is successful, False otherwise.
+    """
+    
     for i in range(3):
         old_password = getpass.getpass(prompt)
         if hashing(old_password) == userbase[username][1]:
